bd6f46cff05eba8bf6c51c42bfec3af7722d1f07
Trung Nguyen <me@ngthanhtrung.com>
2018-08-21 00:38:46 +0700

    Implement a deployer with gas estimation and transaction retries

---
+++ src/deployer.js
@@ -0,0 +1,31 @@
+const logger = require('./logger');
+
+exports.deploy = async (contract, ...args) => {
+  const name = contract._json.contractName;
+  logger.info(`Deploying ${name}...`);
+
+  const instance = await execute(contract.new, ...args);
+  logger.info(`${name}: ${instance.address}`);
+
+  return instance;
+};
+
+const execute = exports.execute = async (tx, ...args) => {
+  try {
+    const gas = await tx.estimateGas(...args);
+    const promiEvent = tx(...args, { gas });
+
+    promiEvent.on('transactionHash', txHash => {
+      logger.info(`... ${txHash}`);
+    });
+
+    return await promiEvent;
+  } catch (err) {
+    if (err.message.match(/(Invalid JSON RPC response|nonce too low)/)) {
+      await new Promise(resolve, setTimeout(resolve, 1000));
+      return await execute(tx, ...args);
+    }
+
+    throw err;
+  }
+};
