2d43fabd0931a8eb8806b8c36b63f7b5fc3d423d
Trung Nguyen <me@ngthanhtrung.com>
2018-08-21 01:07:12 +0700

    Add truffle-deploy binary

---
+++ bin/truffle-deploy.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+if (require.main === module) {
+  const { spawnSync } = require('child_process');
+  const { join } = require('path');
+
+  const argv = [
+    join(__dirname, '../src/deploy.js'),
+    ...process.argv.slice(2),
+  ];
+
+  const { status } = spawnSync('npx truffle exec', argv, {
+    stdio: 'inherit',
+    shell: true,
+  });
+
+  process.exit(status);
+}
+
+module.exports = require('../src/deploy');
