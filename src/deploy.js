cf032a538cd62f5e7823dbbf2d8a1fd05c15f63b
Trung Nguyen <me@ngthanhtrung.com>
2018-08-21 01:02:50 +0700

    Implement a deployment flow with the help of truffle exec

---
+++ src/deploy.js
@@ -0,0 +1,39 @@
+const { join } = require('path');
+
+const deployer = require('./deployer');
+const logger = require('./logger');
+
+module.exports = async callback => {
+  try {
+    const { _, network } = artifacts.options;
+
+    if (_.length <= 1) {
+      throw new Error('A deployment must be specified.');
+    }
+
+    const path = _[1];
+    const accounts = await web3.eth.getAccounts();
+
+    const env = {
+      accounts,
+      artifacts: {
+        require: (...args) => artifacts.require(...args),
+      },
+      deployer,
+      logger,
+      network,
+      web3,
+    };
+
+    const run = require(join(process.cwd(), path));
+
+    console.log(`Running migration: ${path}`);
+    console.log();
+
+    await run(env);
+
+    callback();
+  } catch (err) {
+    callback(err);
+  }
+};
