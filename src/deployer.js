69f1d009be63b0ae2ec74e6b6e7b7f6c3c7020df
Trung Nguyen <me@ngthanhtrung.com>
2018-08-21 03:20:13 +0700

    Add MIT license

--- src/deployer.js
+++ src/deployer.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-present, Axie Infinity, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const logger = require('./logger');
 
 exports.deploy = async (contract, ...args) => {
